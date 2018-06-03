6b07918e6d2397248c6f4c9508b756ddc2c1acc4
jctran <tran.jclaude@gmail.com>
2018-06-04 00:12:15 +0200

    add toggle function in toggle.js and include it in template

---
+++ public/js/toggle.js
@@ -0,0 +1,9 @@
+function toggle() {
+  dialog = document.getElementById("toggleDialog")
+  if(dialog.style.display === "none"){
+    dialog.style.display = "block"
+  }
+  else {
+    dialog.style.display = "none"
+  }
+}
